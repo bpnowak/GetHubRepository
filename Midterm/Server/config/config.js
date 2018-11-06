7d47c830c1ed71fbc7b2c7bf25774f04faa92efd
bpnowak <bpnowak@uwm.edu>
2018-11-05 19:17:42 -0600

    v4

--- Midterm/Server/config/config.js
+++ Midterm/Server/config/config.js
@@ -7,11 +7,21 @@ var config = {
         root: rootPath,
         app: { name: 'UCCSS' },
         port: 3300,
+        db: 'mongodb://127.0.0.1/helpMe-dev'
+
+    },
+    test: {
+        root: rootPath,
+        app: { name: 'UCCSS' },
+        port: 4000,
+        db: 'mongodb://127.0.0.1/helpMe-test'
+    
     },
     production: {
         root: rootPath,
         app: { name: 'UCCSS' },
         port: 80,
+        db: 'mongodb://127.0.0.1/helpMe'
     }
 };
 
--- Midterm/Server/config/express.js
+++ Midterm/Server/config/express.js
@@ -2,15 +2,32 @@ var express = require('express');
 var morgan = require('morgan');
 var logger = require('./logger');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var bluebird = require('bluebird');
 
 module.exports = function (app, config) {
 
-    app.use(function (req, res, next) {
-        logger.log('info', 'Request from ' + req.connection.remoteAddress);
-        next();
+    logger.log('info', "Loading Mongoose functionality");
+    mongoose.Promise = bluebird;
+    mongoose.connect(config.db);
+    var db = mongoose.connection;
+    db.on('error', function () {
+        throw new Error('unable to connect to database at ' + config.db);
     });
 
-    app.use(morgan('dev'));
+    if (process.env.NODE_ENV !== 'test') {
+        app.use(morgan('dev'));
+
+        mongoose.set('debug', true);
+        mongoose.connection.once('open', function callback() {
+            logger.log('info', 'Mongoose connected to the database');
+        });
+
+        app.use(function (req, res, next) {
+            logger.log('Request from ' + req.connection.remoteAddress, 'info');
+            next();
+        });
+    }
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
@@ -19,7 +36,7 @@ module.exports = function (app, config) {
 
     app.use(express.static(config.root + '/public'));
 
-    // require('../app/models/foobar');
+    require('../app/models/foobar');
     require('../app/controllers/foobar')(app, config);
 
     app.use(function (req, res) {
