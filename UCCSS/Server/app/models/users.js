5ff530e16ee9851e99671451dffd877340956cc5
bpnowak <bpnowak@uwm.edu>
2018-11-21 22:09:19 -0600

    18Nov2018v3

--- UCCSS/Server/app/models/users.js
+++ UCCSS/Server/app/models/users.js
@@ -11,7 +11,7 @@ var userSchema = new Schema({
     password: { type: String, require: true }
 });
 
-userSchema.virtual('fullName')
+UserSchema.virtual('fullName')
     .get(function() {
         return this.firstname + ' ' + this.lastname;
     });
