3a7a1d716b2faba433e165c883ee8a1daab31c20
bpnowak <bpnowak@uwm.edu>
2018-12-02 22:17:48 -0600

    misc

--- Midterm/Server/config/express.js
+++ Midterm/Server/config/express.js
@@ -4,12 +4,9 @@ var logger = require('./logger');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var bluebird = require('bluebird');
-var glob = require('glob');
-var cors = require('cors');
 
 module.exports = function (app, config) {
 
-    app.use(cors({ origin: 'http://localhost:9000' }));
     logger.log('info', "Loading Mongoose functionality");
     mongoose.Promise = bluebird;
     mongoose.connect(config.db);
@@ -39,35 +36,21 @@ module.exports = function (app, config) {
 
     app.use(express.static(config.root + '/public'));
 
-    // require('../app/models/users');
-    // require('../app/controllers/users')(app, config);
-
-    var models = glob.sync(config.root + '/app/models/*.js');
-    models.forEach(function (model) {
-        require(model);
-    });
-
-    var controllers = glob.sync(config.root + '/app/controllers/*.js');
-    controllers.forEach(function (controller) {
-        require(controller)(app, config);
-    });
+    require('../app/models/foobar');
+    require('../app/controllers/foobar')(app, config);
 
     app.use(function (req, res) {
-        logger.log('error', 'File not found');
+        // logger.log('error', 'File not found');
         res.type('text/plan');
         res.status(404);
         res.send('404 Not Found');
     });
 
     app.use(function (err, req, res, next) {
-        console.log(err);
-        if (process.env.NODE_ENV !== 'test') logger.log(err.stack, 'error');
+        console.error(err.stack);
         res.type('text/plan');
-        if (err.status) {
-            res.status(err.status).send(err.message);
-        } else {
-            res.status(500).send('500 Sever Error');
-        }
+        res.status(500);
+        res.send('500 Sever Error');
     });
 
     logger.log('info', "Starting application");
